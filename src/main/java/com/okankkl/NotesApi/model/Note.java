e898975f11123166a754b7d910c7e0b52af2adca
okankukul <okankukull@gmail.com>
2025-07-09 14:51:03 +0300

    refactor(lombok): add lombok annotation

--- src/main/java/com/okankkl/NotesApi/model/Note.java
+++ src/main/java/com/okankkl/NotesApi/model/Note.java
@@ -1,6 +1,10 @@
 package com.okankkl.NotesApi.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -9,6 +13,10 @@ import java.time.LocalDateTime;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,48 +29,4 @@ public class Note {
 
     @LastModifiedDate
     private LocalDateTime updatedDate;
-
-    public Note(){
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getHeader() {
-        return header;
-    }
-
-    public void setHeader(String header) {
-        this.header = header;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public LocalDateTime getUpdatedDate() {
-        return updatedDate;
-    }
-
-    public void setUpdatedDate(LocalDateTime updatedDate) {
-        this.updatedDate = updatedDate;
-    }
 }
--- src/main/java/com/okankkl/NotesApi/model/Response.java
+++ src/main/java/com/okankkl/NotesApi/model/Response.java
@@ -2,35 +2,17 @@ package com.okankkl.NotesApi.model;
 
 import jakarta.annotation.Nullable;
 
-public final class Response<T> {
+public record Response<T>(boolean status, @Nullable T data, @Nullable String errorMsg) {
 
-    private final boolean status;
-    @Nullable private final T data;
-    @Nullable private final String errorMsg;
-
-    private Response(boolean status, @Nullable T data, @Nullable String errorMsg) {
-        this.status = status;
-        this.data = data;
-        this.errorMsg = errorMsg;
+    public static <T> Response<T> success(T data) {
+        return new Response<>(true, data, null);
     }
 
-    public boolean isStatus() { return status; }
-
-    @Nullable
-    public T getData() { return data; }
-
-    @Nullable
-    public String getErrorMsg() { return errorMsg; }
-
-    public static <T> Response<T> success(T data){
-        return new Response<>(true,data,null);
+    public static <T> Response<T> success() {
+        return new Response<>(true, null, null);
     }
 
-    public static <T> Response<T> success(){
-        return new Response<>(true,null,null);
-    }
-
-    public static <T> Response<T> failed(String errorMsg){
-        return new Response<>(false,null,errorMsg);
+    public static <T> Response<T> failed(String errorMsg) {
+        return new Response<>(false, null, errorMsg);
     }
 }
